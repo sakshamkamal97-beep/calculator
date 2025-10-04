03fdaacd6f86da4b6907033ce76333f3e7b0051f
saksham kamal <sakshamkamal97@gmail.com>
2025-10-04 15:46:46 +0530

    make js code

---
+++ index.js
@@ -0,0 +1,21 @@
+const display=document.getElementById("display");
+const expression=document.getElementById("expression");
+
+let currExp="";
+let lastInputisOperator=false;
+
+document.addEventListener("keydown",(event)=>{
+    const key = event.key;
+    if(!isNaN(key) || key === "."){
+
+    }
+});
+
+function appendnumber(key){
+    //if last input was operator clear the main display
+    if(lastInputisOperator){
+        display.value = "";
+        lastInputisOperator=false;
+    }
+
+}
